4b26a7a78b505e222b4f6dee66a9d1674a5e946f
syk <2871686782@qq.com>
2020-03-06 14:00:14 +0800

    实现基本的 word 转 pdf

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import os
 import configparser
 import json
 
+# 第三方模块
+from win32com.client import gencache, constants
+
 # 全局变量
 g_config = None
 
 
-def get_config():
+def read_config():
     '''
     (1)读取配置文件
     '''
@@ -22,6 +25,31 @@ def word2pdf():
     (1) 转换指定目录下的 word 为 pdf
     '''
     print('---正在转换为 pdf 文件...')
+    path = os.getcwd() or g_config['path'] or './'
+    # output = g_config['output'] or 'output'
+    file_list = os.listdir(path)
+    word_list = [
+        name for name in file_list if name.endswith(('.doc', '.docx'))]
+    pdf_list = [name for name in file_list if name.endswith('.pdf')]
+    print(word_list)
+    word = gencache.EnsureDispatch('Word.Application')
+    for word_name in word_list:
+        pdf_name = os.path.splitext(word_name)[0]+'.pdf'
+        if pdf_name in pdf_list:
+            continue
+        word_path = path+'\\'+word_name
+        pdf_path = path+'\\'+pdf_name
+        print(pdf_path)
+        try:
+            doc = word.Documents.Open(word_path, ReadOnly=1)
+            doc.ExportAsFixedFormat(pdf_path,
+                                    constants.wdExportFormatPDF,
+                                    Item=constants.wdExportDocumentWithMarkup,
+                                    CreateBookmarks=constants.wdExportCreateHeadingBookmarks)
+        except Exception as e:
+            print('---转换失败，异常是：{}'.format(e))
+        finally:
+            word.Quit(constants.wdDoNotSaveChanges)
     print('---转换完毕')
 
 
@@ -37,7 +65,7 @@ def main():
     '''
     (3) 程序入口
     '''
-    get_config()
+    read_config()
     print('---请输入以下序号选择功能（目录默认为程序所在的当前目录，可在 conf.ini 中进行修改）：')
     print('---1.转换指定目录下的 word 为 pdf')
     print('---2.读取指定目录下文件名称，存为 json 文件')
