f735aea7b2017a9f795158734130d69e300c85bd
syk <2871686782@qq.com>
2020-03-06 14:59:54 +0800

    完成 word 转 pdf ，以及文件名称的提取

--- main.py
+++ main.py
@@ -5,28 +5,28 @@ import json
 # 第三方模块
 from win32com.client import gencache, constants
 
+# 全局变量
+g_path = ''
+g_output_path = ''
+
 
 def word2pdf():
     '''
     (1) 转换当前目录下的 word 为 pdf
     '''
-    folder = './output'
-    if not os.path.isdir(folder):
-        os.mkdir(folder)
-    print('---正在转换为 pdf 文件...')
-    path = os.getcwd()
-    print(path)
-    file_list = os.listdir(path)
-    word_list = [
-        name for name in file_list if name.endswith(('.doc', '.docx'))]
-    pdf_list = [name for name in file_list if name.endswith('.pdf')]
-    word = gencache.EnsureDispatch('Word.Application')
+    if not os.path.isdir(g_output_path):
+        os.mkdir(g_output_path)  # 创建目录
+    names = os.listdir(g_path)
+    word_list = [name for name in names if name.endswith(('.doc', '.docx'))]
+    pdf_list = [name for name in names if name.endswith('.pdf')]
+    word = gencache.EnsureDispatch('Word.Application')  # 调用 word 的 API
     for word_name in word_list:
+        print('---开始转换：', word_name)
         pdf_name = os.path.splitext(word_name)[0]+'.pdf'
         if pdf_name in pdf_list:
             continue
-        word_path = path+'\\'+word_name
-        pdf_path = path+'\\'+folder+'/'+pdf_name
+        word_path = g_path+'/' + word_name
+        pdf_path = g_output_path+'/'+pdf_name
         try:
             doc = word.Documents.Open(word_path, ReadOnly=1)
             doc.ExportAsFixedFormat(pdf_path,
@@ -36,8 +36,9 @@ def word2pdf():
         except Exception as e:
             print('---转换失败，异常是：{}'.format(e))
         finally:
-            word.Quit(constants.wdDoNotSaveChanges)
-    print('---转换完毕')
+            print('---转换成功：', pdf_name)
+    word.Quit(constants.wdDoNotSaveChanges)  # 退出
+    print('---全部转换完毕')
 
 
 def name2json():
@@ -45,6 +46,11 @@ def name2json():
     (2) 读取当前目录下的文件名称，存为 json 文件
     '''
     print('---正在生成 json 文件...')
+    files = os.listdir(g_path)
+    names = [os.path.splitext(file)[0] for file in files]
+    obj = {'files': files, 'names': names}
+    with open(f'output.json', 'w', encoding='utf-8') as f:
+        f.write(json.dumps(obj, ensure_ascii=False))
     print('---生成完毕')
 
 
@@ -52,17 +58,17 @@ def main():
     '''
     (3) 程序入口
     '''
+    global g_path
+    global g_output_path
+    g_path = os.getcwd()
+    g_output_path = g_path+'/output'
     print('---请输入以下序号选择功能：')
     print('---1.转换当前目录下的 word 为 pdf')
     print('---2.读取当前目录下文件名称，存为 json 文件')
-    print('---3.功能 1 和 2 同时进行')
     option = input()
     if option == '1':
         word2pdf()
     elif option == '2':
-        name2json()
-    elif option == '3':
-        word2pdf()
         name2json()
 
 
